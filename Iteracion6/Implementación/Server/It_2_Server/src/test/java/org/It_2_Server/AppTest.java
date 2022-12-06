1e8a5d12297c6a41a805192a8c36f674bad462df
trykill <hector.alberca@alu.uclm.es>
2022-12-06 19:28:36 +0100

    Implementacion server it 6

---
+++ Iteracion6/Implementación/Server/It_2_Server/src/test/java/org/It_2_Server/AppTest.java
@@ -0,0 +1,20 @@
+package org.It_2_Server;
+
+
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+
+    public void shouldAnswerWithTrue()
+    {
+        
+    }
+}
